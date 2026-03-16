7e68e19603ee1327ef0577a88f20e1cafcf2e648
akhiravictoriasalahid-web <akhiravictoriasalahid@gmail.com>
2026-03-16 14:35:59 +0100

    Add JS1 Exercise 1 and Exercise 2

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+//Exercise 1
+console.log("Hello, World!")
+
+//Exercise 2
+let name = "Junie";
+let age = 40;
+let favoriteColor = "Green";
+
+console.log("My name is " + name + ", I am " + age + " years old, and my favorite color is " + favoriteColor + ".");
