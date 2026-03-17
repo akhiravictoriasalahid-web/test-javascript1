97b58b141bd55655ff0ad8053b999ce963816693
akhiravictoriasalahid-web <akhiravictoriasalahid@gmail.com>
2026-03-17 10:27:22 +0100

    Add JS1 Exercise 3, updated JS

--- javascript/index.js
+++ javascript/index.js
@@ -2,8 +2,17 @@
 console.log("Hello, World!")
 
 //Exercise 2
-let name = "Junie";
+let firstName = "Junie";
 let age = 40;
 let favoriteColor = "Green";
 
-console.log("My name is " + name + ", I am " + age + " years old, and my favorite color is " + favoriteColor + ".");
+console.log(`My name is ${firstName}, I am ${age} years old, and my favorite color is ${favoriteColor}.`);
+
+//Exercise 3
+let num1 = 20;
+let num2 = 50;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
